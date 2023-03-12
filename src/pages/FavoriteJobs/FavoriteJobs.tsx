a52d415197a7b1e9a0624a8a580fc0efa99e9b0b
bbogum19 <berkaye2001@hotmail.com>
2023-03-12 17:03:06 +0300

    The FavoriteJobs page has been created.

---
+++ src/pages/FavoriteJobs/FavoriteJobs.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface FavoriteJobs {
+
+}
+
+const FavoriteJobs: React.FC<FavoriteJobs> = () => {
+    return (
+        <View>
+            <Text> Hello Favorite Jobs Page!</Text>
+        </View>
+    )
+}
+
+export default FavoriteJobs;
