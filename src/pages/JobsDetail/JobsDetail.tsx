cca662a18a8ddc556d12095e67cfce377efe2a8c
bbogum19 <berkaye2001@hotmail.com>
2023-03-12 17:02:55 +0300

    The JobsDetail page has been created.

---
+++ src/pages/JobsDetail/JobsDetail.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface JobsDetail {
+
+}
+
+const JobsDetail: React.FC<JobsDetail> = () => {
+    return (
+        <View>
+            <Text> Hello Detail Page!</Text>
+        </View>
+    )
+}
+
+export default JobsDetail;
